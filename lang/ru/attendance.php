9033d58507ff9708a75d83aaf7a1d174daaff4df
den1s0v <hello-lumen@ya.ru>
2026-01-19 13:23:34 +0300

    lang: try add only new lang strings for 'ru'

---
+++ lang/ru/attendance.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'attendance', language 'ru'
+ *
+ * @package   mod_attendance
+ * @copyright  2011 Artem Andreev <andreev.artem@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['enablelimitsessionspergroup'] = 'Разрешить ограничение занятий по группе';
+$string['enablelimitsessionspergroup_desc'] = 'Если включено, позволяет устанавливать ограничение на количество занятий для каждой группы на уровне активности.';
+$string['limitsessionspergroup'] = 'Ограничение занятий по группе';
+$string['limitsessionspergroup_help'] = 'Установите максимальное количество занятий, которые могут быть созданы для каждой группы. Выберите "Без ограничения" для неограниченного количества занятий или "Максимум одно занятие у группы" для ограничения каждой группы одним занятием.';
+$string['nolimit'] = 'Без ограничения';
+$string['maxonesessionpergroup'] = 'Максимум одно занятие у группы';
+$string['hassessions'] = 'есть занятия';
+$string['groupswithsessionsinfo'] = 'Группы с существующими занятиями: {$a}';
+$string['limitsessionspergroupexceeded'] = 'Невозможно создать занятия: следующие группы уже имеют максимально допустимое количество занятий: {$a}';
