c2e3add5f6d2debb2c2ba6afb63c8dff8211b16c
den1s0v <hello-lumen@ya.ru>
2026-02-10 15:46:59 +0300

    chore: bump version

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024082901;
-$plugin->release = 2024082901;
+$plugin->version  = 2024083001;
+$plugin->release = 2024083001;
 $plugin->requires = 2024092700; // Requires 4.5.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
