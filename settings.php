42edf3c7fe316b4d5797b140da4ec25329e19967
den1s0v <hello-lumen@ya.ru>
2026-02-10 16:53:11 +0300

    feat(settings): add flag show-session-creator-in-tooltip

--- settings.php
+++ settings.php
@@ -167,6 +167,13 @@ if ($ADMIN->fulltree) {
     ));
 
     $settings->add(new admin_setting_configcheckbox(
+        'attendance/showsessioncreatorintooltip',
+        get_string('showsessioncreatorintooltip', 'attendance'),
+        get_string('showsessioncreatorintooltip_desc', 'attendance'),
+        1
+    ));
+
+    $settings->add(new admin_setting_configcheckbox(
         'attendance/enablewarnings',
         get_string('enablewarnings', 'attendance'),
         get_string('enablewarnings_desc', 'attendance'),
